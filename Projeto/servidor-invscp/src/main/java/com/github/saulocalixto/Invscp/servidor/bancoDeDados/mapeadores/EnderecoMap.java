3114ea7d47a236b65cab7c55d2399fcf7730747f
marcosrafaellsousa <marcosrafaellsousa@gmail.com>
2018-11-20 20:59:01 -0200

    SERVIDOR: classes de repositorio Endereco

---
+++ Projeto/servidor-invscp/src/main/java/com/github/saulocalixto/Invscp/servidor/bancoDeDados/mapeadores/EnderecoMap.java
@@ -0,0 +1,9 @@
+package com.github.saulocalixto.Invscp.servidor.bancoDeDados.mapeadores;
+
+public abstract class EnderecoMap extends ModelPadraoMap {
+    public static String nomeTabela = "Endereco";
+    public static String rua =  "rua";
+    public static String cidade = "cidade";
+    public static String cep = "cep";
+    public static String bairro = "bairro";
+}
---
+++ Projeto/servidor-invscp/src/main/java/com/github/saulocalixto/Invscp/servidor/bancoDeDados/repositorio/RepositorioEndereco.java
@@ -0,0 +1,30 @@
+package com.github.saulocalixto.Invscp.servidor.bancoDeDados.repositorio;
+
+import com.github.saulocalixto.Invscp.servidor.bancoDeDados.repositorio.interfaces.IRepositorioEndereco;
+import com.github.saulocalixto.Invscp.servidor.negocio.Endereco;
+
+import java.util.List;
+
+public class RepositorioEndereco extends RepositorioPadrao<Endereco> implements IRepositorioEndereco {
+
+
+    public Endereco Consultar(String id) {
+        return null;
+    }
+
+    public List<Endereco> ConsultarLista() {
+        return null;
+    }
+
+    public void Salvar(Endereco objeto) {
+
+    }
+
+    public void Atualizar(Endereco objeto) {
+
+    }
+
+    public void Excluir(String id) {
+
+    }
+}
