34abf45ce41408b5323b110a3c279ed3ca38b456
gustavo-b <gustavo.hsbatista@gmail.com>
2018-11-28 02:37:16 -0200

    BD: Iniciando carga de dados no banco de dados

---
+++ Projeto/servidor-invscp/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+USE invscp;
+
+INSERT INTO Usuario (id, grupo, email, senha, nome, cpf) 
+	VALUES (
+		'seilaquediabosfazisso',
+		'ADMINISTRADOR_DEPARTAMENTO',
+		'admin@admin.com',
+		'$2a$04$BBoGbYsVvFmu/TkxVA18k.5cuAbGy/JCXWVJgRJRAs.bHk2AJicEC',
+		'admin',
+		'02883758077'
+	);
