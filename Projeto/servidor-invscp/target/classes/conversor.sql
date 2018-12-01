cb6e9c471eab926ef8eb22efe11acb4a1cc48de3
Saulo Calixto <sauloacalixto@gmail.com>
2018-12-01 14:36:50 -0200

    Removendo arquivos desnecessários e arrumando gitignore.

--- Projeto/servidor-invscp/target/classes/conversor.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE Usuario ADD COLUMN idDepartamento varchar(40) NULL;
-ALTER TABLE Usuario  ADD FOREIGN KEY (idDepartamento)
-      REFERENCES Departamento(id);
-ALTER TABLE Departamento ADD COLUMN nome varchar(60) NOT NULL;
-ALTER TABLE Sala MODIFY COLUMN idDepartamento varchar(40) NULL;
-ALTER TABLE Sala MODIFY COLUMN idPredio varchar(40) NULL;
