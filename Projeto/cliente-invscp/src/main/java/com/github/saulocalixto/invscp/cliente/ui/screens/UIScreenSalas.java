6b4e34efb75cbf657259bc3615fd9aced9aafab8
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-12-03 16:09:12 -0200

    CLIENTE: SalaAPI com VisualizarSala funcionando

--- Projeto/cliente-invscp/src/main/java/com/github/saulocalixto/invscp/cliente/ui/screens/UIScreenSalas.java
+++ Projeto/cliente-invscp/src/main/java/com/github/saulocalixto/invscp/cliente/ui/screens/UIScreenSalas.java
@@ -5,10 +5,17 @@
  */
 package com.github.saulocalixto.invscp.cliente.ui.screens;
 
+import com.github.saulocalixto.invscp.cliente.api.SalaAPI;
+import com.github.saulocalixto.invscp.cliente.api.UsuarioAPI;
+import com.github.saulocalixto.invscp.cliente.ui.IO;
 import com.github.saulocalixto.invscp.cliente.ui.TerminalUI;
 import com.github.saulocalixto.invscp.cliente.ui.UIScreen;
 import com.github.saulocalixto.invscp.cliente.ui.UIScreenOption;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONObject;
 
 /**
  *
@@ -24,12 +31,23 @@ public class UIScreenSalas extends UIScreen{
             System.out.println("Funcionalidade não implementada");
         }));
         opcoes.put(2, new UIScreenOption("Visualizar", () -> {
+            try {
+                visualizarSala();
+            } catch (IOException ex) {
+                Logger.getLogger(UIScreenSalas.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }));
+        opcoes.put(3, new UIScreenOption("Visualizar Todos", () -> {
+            try {
+                visualizarSalas();
+            } catch (IOException ex) {
+                Logger.getLogger(UIScreenSalas.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }));
+        opcoes.put(4, new UIScreenOption("Editar", () -> {
             System.out.println("Funcionalidade não implementada");
         }));
-        opcoes.put(3, new UIScreenOption("Editar", () -> {
-            System.out.println("Funcionalidade não implementada");
-        }));
-        opcoes.put(4, new UIScreenOption("Deletar", () -> {
+        opcoes.put(5, new UIScreenOption("Deletar", () -> {
             System.out.println("Funcionalidade não implementada");
         }));
     }
@@ -38,4 +56,17 @@ public class UIScreenSalas extends UIScreen{
         super("Salas", opcoes);
     }
     
+    public static void visualizarSala() throws IOException {
+        final String id = IO.readString("Insira o id da Sala:");
+        final String json = SalaAPI.getSala(id);
+
+        JSONObject obj = new JSONObject(json);
+        System.out.println("\nNúmero: " + obj.get("numeroSala"));
+        System.out.println("Prédio: " + obj.get("idPredio"));
+        System.out.println("Departamento: " + obj.get("idDepartamento"));
+    }
+    
+    public static void visualizarSalas() throws IOException {
+        System.out.println(SalaAPI.getSalas());
+    }
 }
