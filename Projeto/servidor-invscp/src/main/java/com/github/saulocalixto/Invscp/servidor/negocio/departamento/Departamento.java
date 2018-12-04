86093e8b8e68e6d87278c7f41b5e5e7c7d48fcd2
gustavo-b <gustavo.hsbatista@gmail.com>
2018-12-04 13:26:14 -0200

    SERVIDOR: Altera serialização do Departamento

--- Projeto/servidor-invscp/src/main/java/com/github/saulocalixto/Invscp/servidor/negocio/departamento/Departamento.java
+++ Projeto/servidor-invscp/src/main/java/com/github/saulocalixto/Invscp/servidor/negocio/departamento/Departamento.java
@@ -1,7 +1,9 @@
 package com.github.saulocalixto.Invscp.servidor.negocio.departamento;
 
+import com.github.saulocalixto.Invscp.servidor.bancoDeDados.repositorio.RepositorioSala;
 import com.github.saulocalixto.Invscp.servidor.negocio.ModelPadrao;
 import com.github.saulocalixto.Invscp.servidor.negocio.sala.Sala;
+import java.util.ArrayList;
 
 import java.util.List;
 
@@ -12,7 +14,7 @@ public class Departamento extends ModelPadrao {
 
     private String nomeDoDepartamento;
 
-    private List<Sala> listaDeSalas;
+    private List<String> listaDeSalas;
 
     public String getNomeDoDepartamento() {
         return nomeDoDepartamento;
@@ -23,10 +25,19 @@ public class Departamento extends ModelPadrao {
     }
 
     public List<Sala> getListaDeSalas() {
+        RepositorioSala repo = new RepositorioSala();
+        List<Sala> listaDeSalas = new ArrayList<Sala>();
+        for (String s : this.listaDeSalas) {
+            listaDeSalas.add(repo.Consultar(s));
+        }
         return listaDeSalas;
     }
 
     public void setListaDeSalas(List<Sala> listaDeSalas) {
-        this.listaDeSalas = listaDeSalas;
+        List<String> salasString = new ArrayList<String>();
+        for (Sala s : listaDeSalas) {
+            salasString.add(s.getId());
+        }
+        this.listaDeSalas = salasString;
     }
 }
