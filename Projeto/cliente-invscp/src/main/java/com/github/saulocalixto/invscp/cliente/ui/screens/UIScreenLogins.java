683a9ed3996a107ae2822d7ba954cdc6cbd092ae
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-12-04 12:51:54 -0200

    CLIENTE: Base das UIScreens restantes

---
+++ Projeto/cliente-invscp/src/main/java/com/github/saulocalixto/invscp/cliente/ui/screens/UIScreenLogins.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2018.
+ * Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+package com.github.saulocalixto.invscp.cliente.ui.screens;
+
+import com.github.saulocalixto.invscp.cliente.ui.UIScreen;
+import com.github.saulocalixto.invscp.cliente.ui.UIScreenOption;
+import java.util.HashMap;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Lucas Sampaio Dias
+ */
+public class UIScreenLogins extends UIScreen{
+    
+    private static final HashMap<Integer, UIScreenOption> opcoes;
+    static
+    {
+        opcoes = new HashMap<>();
+        opcoes.put(1, new UIScreenOption("Cadastrar", () -> {
+            cadastrar();
+        }));
+        opcoes.put(2, new UIScreenOption("Visualizar", () -> {
+            visualizar();
+        }));
+        opcoes.put(3, new UIScreenOption("Visualizar Todos", () -> {
+            visualizarTodos();
+        }));
+        opcoes.put(4, new UIScreenOption("Editar", () -> {
+            editar();
+        }));
+        opcoes.put(5, new UIScreenOption("Deletar", () -> {
+            deletar();
+        }));
+    }
+    
+    private static void cadastrar() {
+        System.out.println("Funcionalidade não implementada");
+    }
+    
+    private static void visualizar() {
+        System.out.println("Funcionalidade não implementada");
+    }
+    
+    private static void visualizarTodos() {
+        System.out.println("Funcionalidade não implementada");
+    }
+    
+    private static void editar() {
+        System.out.println("Funcionalidade não implementada");
+    }
+    
+    private static void deletar() {
+        System.out.println("Funcionalidade não implementada");
+    }
+    
+    private static void mostrar(String json) {
+        JSONObject obj = new JSONObject(json);
+        //System.out.println("\nid: " + obj.get("id"));
+        //System.out.println("Nome: " + obj.get("nome"));
+    }
+    
+    public UIScreenLogins() {
+        super("Logins", opcoes);
+    }
+    
+}
