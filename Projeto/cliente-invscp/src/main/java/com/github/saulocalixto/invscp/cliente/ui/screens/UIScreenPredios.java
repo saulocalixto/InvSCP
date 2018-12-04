9ba49f40f81d5cd098117fbbe200ee5af8ed5769
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-12-04 12:24:31 -0200

    CLIENTE: Predio visualizar e visualizarTodos

--- Projeto/cliente-invscp/src/main/java/com/github/saulocalixto/invscp/cliente/ui/screens/UIScreenPredios.java
+++ Projeto/cliente-invscp/src/main/java/com/github/saulocalixto/invscp/cliente/ui/screens/UIScreenPredios.java
@@ -5,10 +5,16 @@
  */
 package com.github.saulocalixto.invscp.cliente.ui.screens;
 
-import com.github.saulocalixto.invscp.cliente.ui.TerminalUI;
+import com.github.saulocalixto.invscp.cliente.api.PredioAPI;
+import com.github.saulocalixto.invscp.cliente.ui.IO;
 import com.github.saulocalixto.invscp.cliente.ui.UIScreen;
 import com.github.saulocalixto.invscp.cliente.ui.UIScreenOption;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  *
@@ -21,17 +27,63 @@ public class UIScreenPredios extends UIScreen{
     {
         opcoes = new HashMap<>();
         opcoes.put(1, new UIScreenOption("Cadastrar", () -> {
-            System.out.println("Funcionalidade não implementada");
+            cadastrar();
         }));
         opcoes.put(2, new UIScreenOption("Visualizar", () -> {
-            System.out.println("Funcionalidade não implementada");
+            try {
+                visualizar();
+            } catch (IOException ex) {
+                Logger.getLogger(UIScreenPredios.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }));
-        opcoes.put(3, new UIScreenOption("Editar", () -> {
-            System.out.println("Funcionalidade não implementada");
+        opcoes.put(3, new UIScreenOption("Visualizar Todos", () -> {
+            try {
+                visualizarTodos();
+            } catch (IOException ex) {
+                Logger.getLogger(UIScreenPredios.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }));
-        opcoes.put(4, new UIScreenOption("Deletar", () -> {
-            System.out.println("Funcionalidade não implementada");
+        opcoes.put(4, new UIScreenOption("Editar", () -> {
+            editar();
         }));
+        opcoes.put(5, new UIScreenOption("Deletar", () -> {
+            deletar();
+        }));
+    }
+    
+    private static void cadastrar() {
+        System.out.println("Funcionalidade não implementada");
+    }
+    
+    private static void visualizar() throws IOException {
+        final String id = IO.readString("Insira o id do Prédio:");
+        final String json = PredioAPI.getPredio(id);
+
+        if (PredioAPI.isJsonValid(json)) {
+            mostrar(new JSONObject(json).getJSONObject("data").toString());
+        }
+    }
+    
+    private static void visualizarTodos() throws IOException {
+        JSONArray array = new JSONObject(PredioAPI.getPredios()).getJSONArray("data");
+
+        for (int i = 0; i < array.length(); i++) {
+            mostrar(array.get(i).toString());
+        }
+    }
+    
+    private static void editar() {
+        System.out.println("Funcionalidade não implementada");
+    }
+    
+    private static void deletar() {
+        System.out.println("Funcionalidade não implementada");
+    }
+    
+    private static void mostrar(String json) {
+        JSONObject obj = new JSONObject(json);
+        System.out.println("\nid: " + obj.get("id"));
+        System.out.println("Nome: " + obj.get("nome"));
     }
     
     public UIScreenPredios() {
