b6d2218edfb780856913ce184a6d101f582e92fa
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-12-04 12:56:51 -0200

    CLIENTE: UIScreenBens apontando para outras screens

--- Projeto/cliente-invscp/src/main/java/com/github/saulocalixto/invscp/cliente/ui/screens/UIScreenBens.java
+++ Projeto/cliente-invscp/src/main/java/com/github/saulocalixto/invscp/cliente/ui/screens/UIScreenBens.java
@@ -5,9 +5,13 @@
  */
 package com.github.saulocalixto.invscp.cliente.ui.screens;
 
+import com.github.saulocalixto.invscp.cliente.ui.TerminalUI;
 import com.github.saulocalixto.invscp.cliente.ui.UIScreen;
 import com.github.saulocalixto.invscp.cliente.ui.UIScreenOption;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,7 +23,30 @@ public class UIScreenBens extends UIScreen{
     static
     {
         opcoes = new HashMap<>();
-        // Será implementado na Etapa 2
+        opcoes.put(1, new UIScreenOption("Bens Patrimoniais", () -> {
+            try {
+                TerminalUI.mostrar(new UIScreenBemPatrimonial());
+            } catch (IOException ex) {
+                Logger.getLogger(UIScreenMenuPrincipal.class.getName()).log(
+                        Level.SEVERE, null, ex);
+            }
+        }));
+        opcoes.put(2, new UIScreenOption("Baixas", () -> {
+            try {
+                TerminalUI.mostrar(new UIScreenBaixa());
+            } catch (IOException ex) {
+                Logger.getLogger(UIScreenMenuPrincipal.class.getName()).log(
+                        Level.SEVERE, null, ex);
+            }
+        }));
+        opcoes.put(3, new UIScreenOption("Ordens de Serviço", () -> {
+            try {
+                TerminalUI.mostrar(new UIScreenOrdemDeServico());
+            } catch (IOException ex) {
+                Logger.getLogger(UIScreenMenuPrincipal.class.getName()).log(
+                        Level.SEVERE, null, ex);
+            }
+        }));
     }
     
     public UIScreenBens() {
